a8270dc5c5d40a4bac0866b06b901cb527f5d08e
Bsssseep <131932227+Bsssseep@users.noreply.github.com>
2024-04-23 21:42:30 -0700

    update hw

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import TopBar from './components/TopBar';
+import StoreListing from './components/StoreListing';
+import Location from './components/Location';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <TopBar />
+      <Location />
+      <StoreListing />
     </div>
   );
 }
